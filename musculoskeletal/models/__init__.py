e623e9c3c809a11eb347345d88d17e2e609564e5
TimmyNic <tim.nicolas@outlook.com>
2023-08-16 17:01:48 +1200

    Update directory structure

--- musculoskeletal/models/__init__.py
+++ musculoskeletal/models/__init__.py
@@ -1,21 +1,20 @@
 import os
 
 
-CURRENT_PATH = os.path.abspath(os.path.dirname(__file__))
-DATA_PATH = os.path.join(CURRENT_PATH, '..', '..', 'data')
+ROOT_PATH = os.path.abspath(os.path.dirname(__file__))
 
 MODEL_DIRECTORIES = ['shape_models']
 MODEL_TYPES = ['.pc']
 
 
 def get_model_path(model_path):
-    return os.path.join(DATA_PATH, model_path)
+    return os.path.join(ROOT_PATH, model_path)
 
 
 def list_models():
     model_paths = []
     for directory in MODEL_DIRECTORIES:
-        directory_path = os.path.join(DATA_PATH, directory)
+        directory_path = os.path.join(ROOT_PATH, directory)
         for file in os.listdir(directory_path):
             is_file = os.path.isfile(os.path.join(directory_path, file))
             is_model = any(model_type in file for model_type in MODEL_TYPES)
